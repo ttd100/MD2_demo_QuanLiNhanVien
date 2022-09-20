25ea1d3acadbffb4ace010632b12b647da04fddf
Your Name <dat054760@gmail.com>
2022-09-20 09:28:16 +0700

    up

---
+++ src/ra/IEmployee.java
@@ -0,0 +1,9 @@
+package ra;
+
+import java.util.Scanner;
+
+public interface IEmployee {
+    public static final float BASIC_SALARY = 1300000;
+    public void inputData(Scanner sc);
+    public void displayData();
+}
